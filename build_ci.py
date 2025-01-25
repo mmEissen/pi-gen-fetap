95347f0c977604f986b7be7823fc781d31716928
Momo Eissenhauer <momo.eissenhauer@gmail.com>
2025-01-25 15:57:52 +0100

    use caching to speed up build process

---
+++ build_ci.py
@@ -0,0 +1,102 @@
+from os import path
+import subprocess
+import os
+from typing import Optional
+
+CACHE_DIR = "/home/momo/usb1/build_cache"
+STAGE_COUNT = 4
+_WORKING_DIR = path.dirname(__file__)
+_PI_WORK_DIR = path.join(_WORKING_DIR, "work", "raspios-bookworm-arm64")
+
+def main():
+    if os.geteuid() != 0:
+        print("Must be root!")
+        exit(1)
+    build_image()
+
+
+def build_image() -> None:
+    for stage in range(STAGE_COUNT):
+        if code_version(stage) == cached_version(stage):
+            run(["touch", path.join(_WORKING_DIR, f"stage{stage}", "SKIP")])
+        else:
+            first_stage_to_build = stage
+            copy_cache(first_stage_to_build)
+            break
+    else:
+        first_stage_to_build = STAGE_COUNT
+    execute(["./build.sh"], env={"CLEAN": "1"})
+    for stage in range(first_stage_to_build, STAGE_COUNT):
+        store_cache(stage)
+
+
+def copy_cache(target_stage: int) -> None:
+    if target_stage == 0:
+        return
+    stage_dir = path.join(_PI_WORK_DIR, f"stage{target_stage}")
+    run(["mkdir", "-p", stage_dir])
+    cache_dir = path.join(CACHE_DIR, f"stage{target_stage - 1}", cached_version(target_stage - 1))
+    run(["cp", "-r", path.join(cache_dir, "*"), stage_dir])
+
+
+def store_cache(stage: int) -> None:
+    # Make sure CACHE_DIR/stage<n>/ exists and is empty
+    stage_cache_dir = path.join(CACHE_DIR, f"stage{stage}")
+    run(["mkdir", "-p", stage_cache_dir])
+    run(["rm", "-rf", path.joint(stage_cache_dir, "*")])
+
+    # Make sure CACHE_DIR/stage<n>/<git-hash> exists
+    git_hash = run(["git", "rev-parse", "HEAD"]).strip()
+    cache_dir = path.join(stage_cache_dir, git_hash)
+    run(["mkdir", "-p", cache_dir])
+
+    # Actually copy the files from the last build to the cache dir
+    build_stage_dir = path.join(_PI_WORK_DIR, f"stage{stage}")
+    run(["cp", "-r", path.join(build_stage_dir, "*"), cache_dir])
+
+
+def code_version(stage: int) -> str:
+    return run(
+        [
+            "git",
+            "log",
+            "-n",
+            "1",
+            "--pretty=format:%H",
+            "--",
+            f"stage{stage}",
+        ]
+    ).strip()
+
+
+def cached_version(stage: int) -> str:
+    stage_cache_dir = path.join(CACHE_DIR, f"stage{stage}")
+    content = os.listdir(stage_cache_dir)
+    if len(content) != 1:
+        return "<cache invalid>"
+    return content[0]
+
+
+def run(command: list[str], env: Optional[dict[str, str]] = None) -> str:
+    result = subprocess.run(
+        command,
+        stdout=subprocess.PIPE,
+        check=True,
+        text=True,
+        env=env,
+        cwd=_WORKING_DIR,
+    )
+    return result.stdout
+
+
+def execute(command: list[str], env: Optional[dict[str, str]] = None) -> str:
+    subprocess.run(
+        command,
+        check=True,
+        env=env,
+        cwd=_WORKING_DIR,
+    )
+
+
+if __name__ == "__main__":
+    main()
