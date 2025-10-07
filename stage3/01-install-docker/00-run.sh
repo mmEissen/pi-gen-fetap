2d7073f15fdd31051767074406755569e47c8898
Momo Eissenhauer <momo.eissenhauer@gmail.com>
2025-10-07 18:18:17 +0200

    use RELEASE variable for release

--- stage3/01-install-docker/00-run.sh
+++ stage3/01-install-docker/00-run.sh
@@ -5,8 +5,6 @@ chmod a+r /etc/apt/keyrings/docker.asc
 
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
-  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  ${RELEASE} stable" | \
   tee /etc/apt/sources.list.d/docker.list > /dev/null
-
-apt-get update
 EOF
