8258c66a78491710e0dae111236819c2b27ef9e7
Momo Eissenhauer <momo.eissenhauer@gmail.com>
2025-10-07 21:04:21 +0200

    pre download docker.list

--- stage3/01-install-docker/00-run.sh
+++ stage3/01-install-docker/00-run.sh
@@ -1,10 +1,11 @@
-on_chroot << EOF
-install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
-chmod a+r /etc/apt/keyrings/docker.asc
+#!/bin/bash -e
 
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
-  ${RELEASE} stable" | \
-  tee /etc/apt/sources.list.d/docker.list > /dev/null
+install -m 0755 -d /etc/apt/keyrings
+install -m 0755 files/docker.asc "${ROOTFS_DIR}/etc/apt/keyrings/"
+install -m 644 files/docker.list "${ROOTFS_DIR}/etc/apt/sources.list.d/"
+sed -i "s/RELEASE/${RELEASE}/g" "${ROOTFS_DIR}/etc/apt/sources.list.d/docker.list"
+sed -i "s/ARCH/$(dpkg --print-architecture)/g" "${ROOTFS_DIR}/etc/apt/sources.list.d/docker.list"
+
+on_chroot << EOF
+apt-get update
 EOF
